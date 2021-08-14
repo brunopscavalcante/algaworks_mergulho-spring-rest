2c8072f0caa49632ba5cf17bd961cb9a5311b368
Bruno Cavalcante <brian.solidus@gmail.com>
2021-08-14 19:19:28 -0300

    initialize project

---
+++ src/main/java/com/empresa/brunolog/BrunologApiApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.brunolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunologApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunologApiApplication.class, args);
+	}
+
+}
