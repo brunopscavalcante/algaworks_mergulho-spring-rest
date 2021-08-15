c3e88a1fccea12deab35d7b6a112ea6855970fbf
Bruno Cavalcante <brian.solidus@gmail.com>
2021-08-14 22:23:25 -0300

    cria ClienteRepository, serviço de consulta por campo - exata e like

--- src/main/java/com/empresa/brunolog/api/controller/ClienteController.java
+++ src/main/java/com/empresa/brunolog/api/controller/ClienteController.java
@@ -2,24 +2,26 @@ package com.empresa.brunolog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.brunolog.domain.model.Cliente;
+import com.empresa.brunolog.domain.repository.ClienteRepository;
 
+//@AllArgsConstructor //alternativa ao @Autowired
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+//		return clienteRepository.findAll();
+//		return clienteRepository.queryByNome("Bru");
+//		return clienteRepository.findByNome("Bruno Pereira");
+		return clienteRepository.findByNomeContaining("Bruno");
 	}
 
 }
---
+++ src/main/java/com/empresa/brunolog/domain/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package com.empresa.brunolog.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.empresa.brunolog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+//	List<Cliente> queryByNome(String nome);
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+
+}
