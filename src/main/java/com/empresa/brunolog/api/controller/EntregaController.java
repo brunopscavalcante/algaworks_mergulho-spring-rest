67d87abec065aede1dc9936dd98b38eb26ec6ddf
Bruno Cavalcante <brian.solidus@gmail.com>
2021-08-15 23:22:07 -0300

    add validações de endpoints de busca para Entrega
    add ValidationGroups, e validações para Entrega, Cliente e Destinatário

--- src/main/java/com/empresa/brunolog/api/controller/EntregaController.java
+++ src/main/java/com/empresa/brunolog/api/controller/EntregaController.java
@@ -1,6 +1,13 @@
 package com.empresa.brunolog.api.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.empresa.brunolog.domain.model.Entrega;
 import com.empresa.brunolog.domain.model.service.EntregaService;
+import com.empresa.brunolog.domain.repository.EntregaRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -17,12 +25,25 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/entregas")
 public class EntregaController {
 	
+	private EntregaRepository entregaRepository;
 	private EntregaService entregaService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Entrega solicitar(@RequestBody Entrega entrega) {
+	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 		return entregaService.solicitar(entrega);
 	}
 
+	@GetMapping
+	public List<Entrega> listar() {
+		return entregaRepository.findAll();
+	}
+	
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+		return entregaRepository.findById(entregaId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
 }
---
+++ src/main/java/com/empresa/brunolog/domain/ValidationGroups.java
@@ -0,0 +1,7 @@
+package com.empresa.brunolog.domain;
+
+public interface ValidationGroups {
+	
+	public interface ClienteId { }
+
+}
