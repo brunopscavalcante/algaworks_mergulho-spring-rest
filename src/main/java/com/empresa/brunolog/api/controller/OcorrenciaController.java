cf6791772790e97fd8e6c7acf9256a05cf9333a2
Bruno Cavalcante <brian.solidus@gmail.com>
2021-08-16 21:34:48 -0300

    add endpoint busca de ocorrencias

--- src/main/java/com/empresa/brunolog/api/controller/OcorrenciaController.java
+++ src/main/java/com/empresa/brunolog/api/controller/OcorrenciaController.java
@@ -1,8 +1,11 @@
 package com.empresa.brunolog.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.empresa.brunolog.api.assembler.OcorrenciaAssembler;
 import com.empresa.brunolog.api.model.input.OcorrenciaInput;
 import com.empresa.brunolog.api.model.input.OcorrenciaModel;
+import com.empresa.brunolog.domain.model.Entrega;
 import com.empresa.brunolog.domain.model.Ocorrencia;
+import com.empresa.brunolog.domain.model.service.EntregaService;
 import com.empresa.brunolog.domain.model.service.OcorrenciaService;
 
 import lombok.AllArgsConstructor;
@@ -26,6 +31,14 @@ public class OcorrenciaController {
 	private OcorrenciaService ocorrenciaService;
 	
 	private OcorrenciaAssembler ocorrenciaAssembler;
+	
+	private EntregaService entregaService;
+	
+	@GetMapping
+	public List<OcorrenciaModel> listar(@PathVariable Long entregaId) {
+		Entrega entrega = entregaService.buscar(entregaId);
+		return ocorrenciaAssembler.toCollectionModel(entrega.getOcorrencias());
+	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
